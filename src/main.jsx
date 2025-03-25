d7c900f2cdc84e1cad540adb884b00ccadf82f0b
Nikolay Gagarinov <feycot@gmail.com>
2025-03-26 01:06:52 +0500

    add Card

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Card from './Card.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+const mountNode = document.getElementById('root');
+const root = ReactDOM.createRoot(mountNode);
+
+root.render(<React.StrictMode><Card /></React.StrictMode>);
