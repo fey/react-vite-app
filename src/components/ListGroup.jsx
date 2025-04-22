a57580fa0f4a2ee96a97eb868457ba9da3940124
Nikolay Gagarinov <feycot@gmail.com>
2025-04-23 01:05:36 +0500

    add ListGroup

---
+++ src/components/ListGroup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class ListGroup extends React.Component {
+  render() {
+    const { children } = this.props;
+    const childrenElements = React.Children.map(children, (child) => (
+      <li className="list-group-item">
+        {child}
+      </li>
+    ));
+    return (
+      <ul className="list-group">{childrenElements}</ul>
+    );
+  }
+}
